0e40c038f04df0951fed93d472b09ae462850e38
Glory Praise Emmanuel <emmaglorypraise@gmail.com>
2021-06-04 14:50:23 -0700

    first commit

---
+++ schemas/trending.js
@@ -0,0 +1,31 @@
+
+export default {
+    name: 'trending',
+    title: 'Trending',
+    type: 'document', 
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'releaseDate',
+            title: 'Release date',
+            type: 'datetime',
+        },
+        {
+            name: 'popularity',
+            title: 'Popularity',
+            type: 'number',
+        },
+        {
+            name: 'poster',
+            title: 'Poster Image',
+            type: 'image',
+            options: {
+            hotspot: true,
+            },
+        },
+    ]
+}
